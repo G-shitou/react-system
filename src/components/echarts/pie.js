210905f5d5c0af322a79db76d71cfc01bedbf725
G-shitou <gshitou@126.com>
2020-02-13 20:57:46 +0800

    echarts添加饼图组件

---
+++ src/components/echarts/pie.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import echarts from 'echarts/lib/echarts';
+import ReactEchartsCore from 'echarts-for-react/lib/core';
+import 'echarts/lib/chart/pie';
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/title';
+import 'echarts/lib/component/legend';
+import init from './utils/common';
+
+export default class Line extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+
+    UNSAFE_componentWillMount() {
+    }
+
+    getOption = () => {
+        /**
+         * option 参数说明
+         * option.title     string     图形标题
+         * option.color     string     标题文字颜色
+         * option.x         array      分类数据
+         * option.y         array      value数据
+         */
+        const data = this.props.option;
+        return {
+            title: init.title(data),
+            legend:{
+                data: data.x,
+                orient: 'vertical',
+                right:0
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: '{a} <br/>{b} : {c} ({d}%)'
+            },
+            series: [{
+                name: data.name || '统计变量',
+                type: 'pie',
+                radius: data.isRing ? ['40%','60%'] : '60%',
+                center: ['50%', '50%'],
+                data: data.y,
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }]
+        };
+    }
+
+    render() {
+        return (
+            <ReactEchartsCore
+                echarts={echarts}
+                option={this.getOption()}
+                notMerge={true}
+                lazyUpdate={true}/>
+        )
+    }
+}
--- src/pages/gallery/echarts/index.js
+++ src/pages/gallery/echarts/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Line from '../../../components/echarts/line';
 import Bar from '../../../components/echarts/bar';
 import AngleBar from '../../../components/echarts/angleBar';
+import Pie from '../../../components/echarts/pie';
 import { Collapse, Row, Col } from 'antd';
 const { Panel } = Collapse;
 
@@ -16,6 +17,11 @@ export default class Echarts extends React.Component {
                     {type:'类型二',value:[30,40,60,70,50,10,20]},
                     {type:'类型三',value:[60,20,10,50,30,70,40]}
                 ]
+            },
+            pie:{
+                x:['周一','周二','周三','周四','周五','周六','周日'],
+                y:[{name:'周一',value:20},{name:'周二',value:40},{name:'周三',value:30},{name:'周四',value:60},
+                {name:'周五',value:50},{name:'周六',value:10},{name:'周日',value:70}]
             }
         }
     }
@@ -117,10 +123,19 @@ export default class Echarts extends React.Component {
                     <Panel header="饼图" key="3">
                         <Row>
                             <Col span={8}>
-                                
+                                <Pie option={{
+                                    title:'饼图',
+                                    x:this.state.pie.x,
+                                    y:this.state.pie.y
+                                }} />
                             </Col>
                             <Col span={8}>
-                                
+                                <Pie option={{
+                                    title:'圆环',
+                                    x:this.state.pie.x,
+                                    y:this.state.pie.y,
+                                    isRing:true
+                                }} />
                             </Col>
                             <Col span={8}>
                                 
